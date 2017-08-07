1030500ac14b769b81f07bd7985d79749e5bfa98
Richard Hauer <richard.hauer@ping-works.com.au>
2017-08-07 18:57:50 +1000

    Remember last used field values in User settings

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DecodeASPXAUTH
 {
-	partial class frmMain
+	partial class MainForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -36,6 +36,9 @@
 			this.txtValue = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
 			this.txtDecryptionKey = new System.Windows.Forms.TextBox();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// txtValidationKey
@@ -74,7 +77,7 @@
 			this.txtCookie.Multiline = true;
 			this.txtCookie.Name = "txtCookie";
 			this.txtCookie.Size = new System.Drawing.Size(733, 82);
-			this.txtCookie.TabIndex = 3;
+			this.txtCookie.TabIndex = 2;
 			this.txtCookie.TextChanged += new System.EventHandler(this.inputField_TextChanged);
 			// 
 			// label3
@@ -95,8 +98,8 @@
 			this.txtValue.Multiline = true;
 			this.txtValue.Name = "txtValue";
 			this.txtValue.ReadOnly = true;
-			this.txtValue.Size = new System.Drawing.Size(733, 192);
-			this.txtValue.TabIndex = 5;
+			this.txtValue.Size = new System.Drawing.Size(733, 167);
+			this.txtValue.TabIndex = 3;
 			// 
 			// label4
 			// 
@@ -114,14 +117,32 @@
 			this.txtDecryptionKey.Location = new System.Drawing.Point(12, 80);
 			this.txtDecryptionKey.Name = "txtDecryptionKey";
 			this.txtDecryptionKey.Size = new System.Drawing.Size(732, 20);
-			this.txtDecryptionKey.TabIndex = 6;
+			this.txtDecryptionKey.TabIndex = 1;
 			this.txtDecryptionKey.TextChanged += new System.EventHandler(this.inputField_TextChanged);
 			// 
-			// frmMain
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 429);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(757, 22);
+			this.statusStrip1.TabIndex = 8;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(489, 17);
+			this.toolStripStatusLabel1.Text = "Enter ValidationKey and DecryptionKey from the <machineKey /> section of the web." +
+    "config";
+			// 
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(757, 451);
+			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.txtDecryptionKey);
 			this.Controls.Add(this.txtValue);
@@ -131,8 +152,11 @@
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.txtValidationKey);
 			this.MinimumSize = new System.Drawing.Size(388, 385);
-			this.Name = "frmMain";
+			this.Name = "MainForm";
 			this.Text = "Decode ASPXAUTH Cookie";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -148,6 +172,8 @@
 		private System.Windows.Forms.TextBox txtValue;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.TextBox txtDecryptionKey;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
 	}
 }
 
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -10,16 +10,24 @@ using System.Windows.Forms;
 
 namespace DecodeASPXAUTH
 {
-	public partial class frmMain : Form
+	public partial class MainForm : Form
 	{
 		private readonly CookieDecoder Decoder;
 
-		public frmMain()
+		public MainForm()
 		{
 			InitializeComponent();
 
 			Decoder = new CookieDecoder();
 			Decoder.Recalculated += Decoder_Recalculated;
+
+			LoadFromSettings();
+		}
+
+		private void LoadFromSettings()
+		{
+			txtDecryptionKey.Text = Properties.Settings.Default.DecryptionKey;
+			txtValidationKey.Text = Properties.Settings.Default.ValidationKey;
 		}
 
 		private void Decoder_Recalculated( string decodedValue )
@@ -32,6 +40,13 @@ namespace DecodeASPXAUTH
 			Decoder.SetCookieValue( txtCookie.Text );
 			Decoder.SetDecryptionKey( txtDecryptionKey.Text );
 			Decoder.SetValidationKey( txtValidationKey.Text );
+		}
+
+		private void frmMain_FormClosing( object sender, FormClosingEventArgs e )
+		{
+			Properties.Settings.Default.DecryptionKey = txtDecryptionKey.Text;
+			Properties.Settings.Default.ValidationKey = txtValidationKey.Text;
+			Properties.Settings.Default.Save();
 		}
 	}
 }
